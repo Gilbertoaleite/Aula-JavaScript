9cb3e98b67240846a923715e01c2aaa092c3ea61
GILBERTO DE ARAUJO LEITE <90141253+Gilbertoaleite@users.noreply.github.com>
2021-09-11 22:24:17 -0300

    Aulas de JavaScript

---
+++ Aula-JavaScript/Introdução a Programação com JavaScript  Folha de Pagamento desafio Dio.js
@@ -0,0 +1,21 @@
+//teste dio
+
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let valor3 = parseFloat(gets());
+
+
+
+let salary = parseFloat(valor2 * valor3).toFixed(2);
+console.log("NUMBER = " + valor1);
+console.log("SALARY = U$ " + salary);
+
+/*
+//para uso no local
+let num = parseInt(25); 
+let hours = parseInt(5); 
+let wage = parseFloat(5.50); 
+let salary = parseFloat(wage * hours).toFixed(2);
+console.log("NUMBER = " + num);
+console.log("SALARY = U$ " + salary);
+*/
---
+++ Aula-JavaScript/JS/main.js
@@ -0,0 +1,120 @@
+ 
+ 
+ function botao(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por se cadastrar! clique aqui para ir para o seu cadastro :) ";
+  
+
+ }
+ 
+
+function redirecionar(){
+window.open("http://globo.com");
+
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "SEJA BEM VINDO!"
+    
+   /*alert("Proibido copiar!!");
+  
+    document.getElementById("mousemove").innerHTML= "Clique aqui para entra na sua conta"
+    */
+
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "seja bem vindo"
+
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+function load() {
+    alert("Pagina carregada!")
+    
+}
+
+/*var validar;
+function validarIdade(idade){
+   
+     if(idade >= 18){
+       validar = "Maior de idade"
+    }else{
+       validar = "Menor de idade"
+    }
+    return validar;
+    
+   }
+
+var idade = prompt( "Qual é a sua idade? ");
+console.log(validarIdade(idade));
+alert(validarIdade(idade));
+*/
+
+
+
+/*
+var n1 = 15;
+var n2 = 15;
+var r = n1+n2;
+ 
+function soma (n1, n2){
+    return (n1 + n2 );
+}
+
+
+function setReplace(frase, nome, novo_nome) {
+      return frase.replace(nome, novo_nome)
+
+
+
+}
+console.log(r);
+alert(r);
+alert(setReplace("Vai Bambis", "Bambis", "Corinthians"));
+
+*/
+
+
+/*
+var d = new Date();
+alert(d.getMonth ()+1);
+*\
+
+
+
+/* var count;
+for(count = 0; count <= 5; count ++ ){
+    alert(count);
+};
+
+
+
+
+var count= 0;
+while (count <= 5){
+    console.log(count);
+count = count ++;
+
+
+};
+*/
+/*
+var idade = prompt("Qual sua idade?")
+if (idade >= 18){
+alert ("Maior de idade");
+}else{ 
+    alert("Menor de idade");
+
+};
+
+var nome = "Gilberto de Araujo Leite";
+//var idade = 35;
+alert ( " Seja bem vindo! " + nome )
+console.log (nome)
+console.log("Tem " + idade + " Anos")
+
+.text1
+*/
